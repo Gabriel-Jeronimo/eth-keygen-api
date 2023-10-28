24a8fae45bba59a0191e70ddd8c35408f707ba68
Gabriel Jeronimo <gabrieljeronimo-@hotmail.com>
2023-10-27 23:01:13 -0300

    Return JSON struct and receive publicKey as query string

--- internal/keypair/handler.go
+++ internal/keypair/handler.go
@@ -1,32 +1,76 @@
 package keypair
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
+
+type GenerateKeypairHandlerResponse struct {
+	PrivateKey string `json:"privateKey"`
+	PublicKey  string `json:"publicKey"`
+}
+
+type GetAddressHandlerResponse struct {
+	Address string `json:"address"`
+}
 
 func InitRoutes() {
 	http.HandleFunc("/keypair", GenerateKeypairHandler)
 	http.HandleFunc("/address", GetAddressHandler)
 }
 
-// TODO: Create the return structure
 func GenerateKeypairHandler(w http.ResponseWriter, r *http.Request) {
 	privateKey, publicKey, err := GenerateKeypair()
 
 	if err != nil {
 		http.Error(w, "Failed to generate a new keypair: "+err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Printf("%q %q", privateKey, publicKey)
+	responseData := GenerateKeypairHandlerResponse{
+		PrivateKey: privateKey,
+		PublicKey:  publicKey,
+	}
+
+	jsonResponse, err := json.Marshal(responseData)
+
+	if err != nil {
+		http.Error(w, "Failed to create JSON response"+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(jsonResponse)
 }
 
 func GetAddressHandler(w http.ResponseWriter, r *http.Request) {
-	address, err := GetAddress("")
+	publicKey := r.URL.Query().Get("publicKey")
+
+	if publicKey == "" {
+		http.Error(w, "Parameter 'publicKey' not provided in the query string", http.StatusBadRequest)
+		return
+	}
+
+	address, err := GetAddress(publicKey)
 
 	if err != nil {
 		http.Error(w, "Failed to retrieve the address from the public key: "+err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Printf("%q", address)
+	responseData := GetAddressHandlerResponse{
+		Address: address,
+	}
+
+	jsonResponse, err := json.Marshal(responseData)
+
+	if err != nil {
+		http.Error(w, "Failed to create JSON response"+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
 }
