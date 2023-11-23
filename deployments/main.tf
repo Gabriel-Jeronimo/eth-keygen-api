a1fb237d7f770e79efe4355f3029e2d2c8266782
Gabriel Jeronimo <gabrieljeronimo-@hotmail.com>
2023-11-23 16:31:08 -0300

    Convert from SAM to Terraform

--- deployments/main.tf
+++ deployments/main.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.36"
+      source  = "hashicorp/aws"
+      version = "~> 4.36"
     }
   }
 }
 
 provider "aws" {
-    region = "us-west-2"
+  region = var.region
 }
-
